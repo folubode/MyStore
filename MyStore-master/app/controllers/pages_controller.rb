8a6a51ad54675ca1555d9ab15fb2635033026d84
Femi Olubode-Awosola <olucube@yahoo.co.uk>
2016-11-19 07:18:35 +1300

    layout page sorted

---
+++ MyStore-master/app/controllers/pages_controller.rb
@@ -0,0 +1,17 @@
+class PagesController < ApplicationController
+	
+	include CurrentCart
+	before_action :set_cart, only: [:index, :shop, :about]
+	
+  def index
+	  @products = Product.all
+  end
+  
+  def shop
+	  @products = Product.all
+	end
+	
+	def about
+	end
+  
+end
---
+++ MyStore-master/db/migrate/20160517073029_add_state_to_orders.rb
@@ -0,0 +1,5 @@
+class AddStateToOrders < ActiveRecord::Migration
+  def change
+    add_column :orders, :shipped, :boolean, default: false
+  end
+end
